6dce13db2f0377065ddf15150e99d81313d9e3fd
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 02:21:07 +0330

    Add root GET endpoint to fetch all todos from the database

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import Annotated
+
 import models
-from database import engine
+from database import SessionLocal, engine
+from models import Todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
