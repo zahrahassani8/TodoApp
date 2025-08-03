5da91b11214c2166bc637d13d2f77d09f5cf98e4
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 03:18:24 +0330

    Add PUT endpoint to update an existing todo by ID.

--- main.py
+++ main.py
@@ -50,3 +50,18 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.dict())
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found.")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.completed = todo_request.completed
+
+    db.add(todo_model)
+    db.commit()
