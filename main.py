5cd27de39d387eb68710756885746a8442e76244
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 02:38:32 +0330

    Refactor root route with dependency annotation for DB session

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -17,6 +18,9 @@ def get_db():
     finally:
         db.close()
 
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
 @app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+async def read_all(db: db_dependency):
     return db.query(Todos).all()
