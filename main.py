a07854a9f908a9515ba2bb6709a8bac472cecc8e
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 02:46:58 +0330

    Add GET endpoint to retrieve a specific todo by ID.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import Annotated
 
@@ -24,3 +24,11 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/")
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(db: db_dependency, todo_id: int):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found.")
