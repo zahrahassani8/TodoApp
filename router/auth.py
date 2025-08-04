1d39c7c9f093abbfdae21d6a33500ef16b26b06f
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 23:00:59 +0330

    Implement user creation endpoint with password hashing.

--- router/auth.py
+++ router/auth.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
+
 
 router = APIRouter()
 
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -18,11 +21,12 @@ class CreateUserRequest(BaseModel):
 @router.post("/auth")
 async def create_user(create_user_request: CreateUserRequest):
     create_user_model = Users(
-        username = create_user_request.username,
-        email = create_user_request.email,
-        first_name = create_user_request.first_name,
-        last_name = create_user_request.last_name,
-        role = create_user_request.role,
-        hashed_password = create_user_request.password,
-        is_active = True)
-    return create_user_request
+        username=create_user_request.username,
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+    return create_user_model
