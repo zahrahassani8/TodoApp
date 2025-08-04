8a305d9946d7084457962c7473ee42c415e90b4b
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 23:13:50 +0330

    Add database session dependency and persist user registration data

--- router/auth.py
+++ router/auth.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
 
 
 router = APIRouter()
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -18,8 +23,19 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         username=create_user_request.username,
         email=create_user_request.email,
@@ -29,4 +45,5 @@ async def create_user(create_user_request: CreateUserRequest):
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
