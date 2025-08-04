89f9282a2648a66884dff8fda1cb430d15ff7449
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 06:12:10 +0330

    Add Users model with SQLAlchemy schema

--- models.py
+++ models.py
@@ -1,5 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default= True)
+    role = Column(String)
+    
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -8,4 +22,5 @@ class Todos(Base):
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    completed = Column(Boolean)
+    completed = Column(Boolean) 
+    owner_id = Column(Integer, ForeignKey('users.id'))
