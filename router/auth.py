85ba95150b3dcfb08956bac323ae5aff34870077
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-04 22:42:07 +0330

    Add user registration endpoint with input validation and DB integration.

--- router/auth.py
+++ router/auth.py
@@ -1,4 +1,28 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
 
 router = APIRouter()
 
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        username = create_user_request.username,
+        email = create_user_request.email,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        role = create_user_request.role,
+        hashed_password = create_user_request.password,
+        is_active = True)
+    return create_user_request
