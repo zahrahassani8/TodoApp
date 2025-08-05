3dc756d387dacef5c27704f969797423956183eb
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-05 03:35:56 +0330

    Add JWT-based authentication and token validation to user system.

--- router/auth.py
+++ router/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
@@ -6,8 +6,8 @@ from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 from datetime import timedelta, datetime
 
 
@@ -19,6 +19,7 @@ ALGORITHM = 'HS256'
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 
 class CreateUserRequest(BaseModel):
@@ -60,6 +61,19 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.utcnow() + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Couldn't validate user.")
+        return {'username': username, 'id': user_id}
+    except JWTError: 
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Couldn't validate user.")
+
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
