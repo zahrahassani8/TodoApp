bee8a1e732d5749058ed9eb7d8d1c569ce25e8a2
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-05 03:52:11 +0330

    Add user authentication to todo creation endpoint.

--- router/todos.py
+++ router/todos.py
@@ -5,7 +5,7 @@ from starlette import status
 from pydantic import BaseModel, Field
 from database import SessionLocal
 from models import Todos
-
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -19,6 +19,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 class TodoRequest(BaseModel):
@@ -42,8 +43,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.dict())
+async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed!")
+    todo_model = Todos(**todo_request.dict(), owner_id=user.get('id'))
     db.add(todo_model)
     db.commit()
 
