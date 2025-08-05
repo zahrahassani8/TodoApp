62bf72b6d7a31cafc247637f1562e315073f0511
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-06 02:25:07 +0330

    add user profile and password update endpoints.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from router import auth, todos, admin
+from router import auth, todos, admin, users
 
 
 app = FastAPI()
@@ -12,6 +12,8 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
+
 
 
 
---
+++ router/users.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from sqlalchemy.orm import Session
+from typing import Annotated
+from starlette import status
+from pydantic import BaseModel, Field
+from database import SessionLocal
+from .auth import get_current_user
+from passlib.context import CryptContext
+from models import Users
+
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed!.")
+    return db.query(Users).filter(Users.id == user.get("id")).first()
+
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, user_verfication: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed!.")
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    if not bcrypt_context.verify(user_verfication.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail="Error on password change!")
+    user_model.hashed_password = bcrypt_context.hash(user_verfication.new_password)
+    db.add(user_model)
+    db.commit()
