032406793b4c7739c56d15e14f6c00e76c7bbe18
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-13 00:19:50 +0330

    adding phone_number to pydantic user model and post endpoint.

--- router/auth.py
+++ router/auth.py
@@ -32,6 +32,7 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    phone_number: str
 
 
 class Token(BaseModel):
@@ -89,7 +90,8 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
         last_name=create_user_request.last_name,
         role=create_user_request.role,
         hashed_password=bcrypt_context.hash(create_user_request.password),
-        is_active=True
+        is_active=True,
+        phone_number=create_user_request.phone_number
     )
     db.add(create_user_model)
     db.commit()
