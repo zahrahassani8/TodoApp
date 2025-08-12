c4e00899bc97a76a63e7479346a89d555d9fc61f
zahrahassani8 <zahrahassanii08@gmail.com>
2025-08-13 01:39:18 +0330

    adding phone_number functionality.

--- router/users.py
+++ router/users.py
@@ -50,3 +50,13 @@ async def change_password(user: user_dependency, db: db_dependency, user_verfica
     user_model.hashed_password = bcrypt_context.hash(user_verfication.new_password)
     db.add(user_model)
     db.commit()
+
+
+@router.put("/phonenumber/{phone_number}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(db: db_dependency, user: user_dependency, phone_number: str):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed!.")
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    user_model.phone_number = phone_number
+    db.add(user_model)
+    db.commit()
